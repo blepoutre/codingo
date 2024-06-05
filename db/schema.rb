898d8007366397606e83ce820959464224effe1a
Mathys Meuris <mathys.meuris@gmail.com>
2024-06-05 12:58:13 +0200

    exo logic + migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_06_05_100155) do
+ActiveRecord::Schema[7.1].define(version: 2024_06_05_105624) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -40,6 +40,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_06_05_100155) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "url"
+    t.text "answers", default: [], array: true
     t.index ["world_id"], name: "index_levels_on_world_id"
   end
 
