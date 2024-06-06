5d9a86461ffe7b98fc47039ebcde2934182ce28a
Mathys Meuris <mathys.meuris@gmail.com>
2024-06-06 12:58:40 +0200

    Exo logic finish

--- app/controllers/levels_controller.rb
+++ app/controllers/levels_controller.rb
@@ -1,5 +1,8 @@
 class LevelsController < ApplicationController
   def show
     @level = Level.find(params[:id])
+    @counter = 0
+    @reponse = ["reponse1", "reponse2", "reponse3", "reponse4", "reponse5"]
+    @user_reponse = 0
   end
 end
--- app/javascript/controllers/answer_controller.js
+++ app/javascript/controllers/answer_controller.js
@@ -2,22 +2,40 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="answer"
 export default class extends Controller {
-  static targets=["reponse1", "reponse2", "reponse3", "reponse4", "reponse5"]
+  static targets=["reponse1", "reponse2", "reponse3", "reponse4", "reponse5", "win", "loose"];
+
+  static values={
+    validation: Number
+  };
 
   check() {
-    if (this.reponse1Target.firstChild.nextSibling.checked){
-      console.log(this.reponse1Target.innerText)}
-    else if (this.reponse2Target.firstChild.nextSibling.checked){
-      console.log(this.reponse2Target.innerText)
+    switch(true) {
+      case (this.reponse1Target.firstChild.nextSibling.checked):
+        this.userAnswer = this.reponse1Target.innerText;
+        break;
+      case (this.reponse2Target.firstChild.nextSibling.checked):
+        this.userAnswer = this.reponse2Target.innerText;
+        break;
+      case (this.reponse3Target.firstChild.nextSibling.checked):
+        this.userAnswer = this.reponse3Target.innerText;
+        break;
+      case (this.reponse4Target.firstChild.nextSibling.checked):
+        this.userAnswer = this.reponse4Target.innerText;
+        break;
+      case (this.reponse5Target.firstChild.nextSibling.checked):
+        this.userAnswer = this.reponse5Target.innerText;
+        break;
     }
-    else if (this.reponse3Target.firstChild.nextSibling.checked){
-      console.log(this.reponse3Target.innerText)
-    }
-    else if (this.reponse4Target.firstChild.nextSibling.checked){
-      console.log(this.reponse4Target.innerText)
-    }
-    else if (this.reponse5Target.firstChild.nextSibling.checked){
-      console.log(this.reponse5Target.innerText)
-    }
+    this.result(this.userAnswer);
+  }
+
+  result(userAnswer){
+    if (parseInt(userAnswer) === this.validationValue){
+        this.winTarget.classList.add("show");
+        this.winTarget.classList.add("d-block");
+    } else{
+        this.looseTarget.classList.add("show");
+        this.looseTarget.classList.add("d-block");
+    };
   }
 }
