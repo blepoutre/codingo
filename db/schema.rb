20a372db8439f03f3025ca561297d32c5719afe1
Mathys Meuris <mathys.meuris@gmail.com>
2024-06-07 09:45:26 +0200

    Add to win to levels

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_06_05_105624) do
+ActiveRecord::Schema[7.1].define(version: 2024_06_07_074317) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -69,6 +69,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_06_05_105624) do
     t.datetime "updated_at", null: false
     t.string "url"
     t.text "answers", default: [], array: true
+    t.boolean "win", default: false
     t.index ["world_id"], name: "index_levels_on_world_id"
   end
 
