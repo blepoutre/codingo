5c62d394ad6b07a73f7498145b77eed8d84f098f
Aline GOMES LOPES <aline.gl21@gmail.com>
2024-06-11 13:00:42 +0200

    Adding characters

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   has_many :collections
+  has_many :parties, dependent: :destroy
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
